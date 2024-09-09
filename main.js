52cc1a688015ac55ba06c97ddaa81f2ec872b0b2
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2024-09-09 19:53:40 -0300

    Answer question functions and refact

--- main.js
+++ main.js
@@ -62,17 +62,7 @@ const startup = {
     setAnswersEvents: function () {
         const answersListDiv = document.getElementById('answerList');
         for (let answerDiv = answersListDiv.children[0]; !utils.isNullOrEmpty(answerDiv); answerDiv = answerDiv.nextElementSibling) {
-            answerDiv.onclick = function () {
-                for (let unselectedAnswer = answersListDiv.children[0]; !utils.isNullOrEmpty(unselectedAnswer); unselectedAnswer = unselectedAnswer.nextElementSibling) {
-                    const answerIcon = unselectedAnswer.getElementsByTagName('i')[0];
-                    answerIcon.classList.replace(stylesClasses.selectedIcon, stylesClasses.unselectedIcon);
-                    unselectedAnswer.classList.remove(stylesClasses.selectedAnswer);
-                }
-
-                const answerIcon = answerDiv.getElementsByTagName('i')[0];
-                answerIcon.classList.replace(stylesClasses.unselectedIcon, stylesClasses.selectedIcon);
-                answerDiv.classList.add(stylesClasses.selectedAnswer);
-            }
+            answerDiv.addEventListener('click', () => { question.setSelectedQuestion(answerDiv) });
         }
     }
 };
@@ -85,14 +75,31 @@ const utils = {
 
 /********** Questions Functions **********/
 const question = {
-    answeredQuestions: [],
     getRandomQuestion: function () {
-        const notAnsweredQuestions = questionsList.filter(x => !this.answeredQuestions.some(y => y == x.id));
+        const notAnsweredQuestions = questionsList.filter(x => !player.answeredQuestions.some(y => y == x.id));
         const randomQuestionIndex = utils.randomIntFromInterval(0, notAnsweredQuestions.length -1);
         
         if(notAnsweredQuestions.length <= 0) console.error('Error: There is no questions left to answer.');
 
         return notAnsweredQuestions[randomQuestionIndex];    
+    },
+    setSelectedQuestion: function (answerDiv) {
+        this.clearSelectedQuestions();
+
+        const answerIcon = answerDiv.getElementsByTagName('i')[0];
+        answerIcon.classList.replace(stylesClasses.unselectedIcon, stylesClasses.selectedIcon);
+        answerDiv.classList.add(stylesClasses.selectedAnswer);
+
+        player.selectedAnswer = answerDiv.answerId;
+    },
+    clearSelectedQuestions: function () {
+        const answersListDiv = document.getElementById('answerList');
+        for (let unselectedAnswer = answersListDiv.children[0]; !utils.isNullOrEmpty(unselectedAnswer); unselectedAnswer = unselectedAnswer.nextElementSibling) {
+            const answerIcon = unselectedAnswer.getElementsByTagName('i')[0];
+            answerIcon.classList.replace(stylesClasses.selectedIcon, stylesClasses.unselectedIcon);
+            unselectedAnswer.classList.remove(stylesClasses.selectedAnswer);
+            player.selectedAnswer = undefined;
+        }
     }
 };
 
@@ -121,6 +128,28 @@ const presentation = {
 };
 
 /********** Game Functions **********/
+const player = {
+    tipsLeft: 0,
+    correctAnswers: 0,
+    answeredQuestions: [],
+    selectedAnswer: undefined,
+    startGame: function () {
+        this.tipsLeft = 3;
+        this.correctAnswers = 0;
+        this.answeredQuestions = [];
+        this.selectedAnswer = undefined;
+        this.selectedQuestion = undefined;
+    },
+    answerQuestion: function () {
+        if (utils.isNullOrEmpty(this.selectedAnswer)) return;
+
+        if (questionsList.flatMap(x => x.answers).find(x => x.id == this.selectedAnswer).correctAnswer)
+            this.correctAnswers++;
+
+        game.nextQuestion();
+    }
+};
+
 const game = {
     start: function () {
         const invisbleDivsIds = ['contentNewGame', 'contentEndGame'];
@@ -129,10 +158,13 @@ const game = {
         invisbleDivsIds.forEach(x => presentation.setVisible(x, false));
         visibleDivs.forEach(x => presentation.setVisible(x, true));
 
+        player.startGame();
         this.nextQuestion();
     },
     nextQuestion: function () {
+        question.clearSelectedQuestions();
         const selectedQuestion = question.getRandomQuestion();
+        player.answeredQuestions.push(selectedQuestion.id);
 
         const questionDiv = document.getElementById('question');
         questionDiv.innerText = selectedQuestion.statment;
@@ -143,6 +175,7 @@ const game = {
             const answerText = answerDiv.getElementsByTagName('span')[0];
 
             answerText.innerText = selectedQuestion.answers[i].description;
+            answerDiv.answerId = selectedQuestion.answers[i].id;
         }
     }
 };
